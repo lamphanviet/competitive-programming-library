8f1cc003aa8b82b130280d0c3b58aa4b4ccd1525
Viet Lam <vietlam@amazon.com>
2018-02-26 10:58:09 +1100

    add template

---
+++ template.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fr(i,a,b) for (int i = (a), _b = (b); i <= _b; i++)
+#define frr(i,a,b) for (int i = (a), _b = (b); i >= _b; i--)
+#define rep(i,n) for (int i = 0, _n = (n); i < _n; i++)
+#define repr(i,n) for (int i = (n) - 1; i >= 0; i--)
+#define foreach(it, ar) for ( typeof(ar.begin()) it = ar.begin(); it != ar.end(); it++ )
+#define fill(ar, val) memset(ar, val, sizeof(ar))
+
+#define ull unsigned long long
+#define ll long long
+#define all(ar) ar.begin(), ar.end()
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+
+#define BIT(n) (1<<(n))
+#define AND(a,b) ((a) & (b))
+#define OR(a,b) ((a) | (b))
+#define XOR(a,b) ((a) ^ (b))
+#define sqr(x) ((x) * (x))
+
+#define ri(a) scanf("%d", &a)
+#define rs(a) scanf("%s", &a)
+
+typedef pair<int, int> ii;
+typedef pair<int, ii> iii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+
+#define PI 3.1415926535897932385
+#define INF 1000111222
+#define eps 1e-7
+#define N 100111
+
+
+int main() {
+	#ifndef ONLINE_JUDGE
+	freopen("inp.txt", "r", stdin);
+	//freopen("out.txt", "w", stdout);
+	#endif
+	
+	return 0;
+}
+// lamphanviet@gmail.com
